0d1a9363a1ce0584143c0ff9bec4801b19f19c1b
Googieee <utuznikov96@gmail.com>
2024-11-28 16:10:13 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using InspectorLib;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inspector = new FunctionInsp();
+
+            Console.WriteLine(inspector.GetCarInspection());
+            Console.WriteLine(inspector.GetInspector());
+
+            // Попробуем установить нового главного инспектора
+            if (inspector.SetInspector("Иванов И.И."))
+            {
+                Console.WriteLine("Главный инспектор изменен на: " + inspector.GetInspector());
+            }
+            else
+            {
+                Console.WriteLine("Не удалось изменить главного инспектора.");
+            }
+
+            // Генерация госномера
+            string number = inspector.GenerateNumber('A', 1234, 75);
+            Console.WriteLine("Сгенерированный госномер: " + number);
+
+            // Вывод списка сотрудников
+            Console.WriteLine("Список сотрудников: ");
+            foreach (var worker in inspector.GetWorker())
+            {
+                Console.WriteLine(worker);
+            }
+
+            // Добавление нового сотрудника
+            inspector.AddWorker("Сидоров С.С.");
+            Console.WriteLine("После добавления нового сотрудника:");
+
+            // Вывод обновленного списка сотрудников
+            foreach (var worker in inspector.GetWorker())
+            {
+                Console.WriteLine(worker);
+            }
+        }
+    }
+}
